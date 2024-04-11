9991f4dd204dd0f7b4d87a4d7a0b87bfc2aa428f
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-10 23:32:36 -0300

    feat: create routes

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+    "/": "Curso de Node.js!",
+    "/cachorro": "Entrei na rota cachorro",
+    "/raca": "Entrei na rota raça"
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     // Head cabeçalho de uma requisição HTTP
-    res.end("Curso de Node.js");
+    res.end(rotas[req.url]);
 })
 
 server.listen(PORT, () => {
