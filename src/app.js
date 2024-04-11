b2664971a35c5c15716df656acc96045f8061bf1
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2024-04-10 23:51:15 -0300

    chore: populate cachorros route

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const cachorros = [
+    {
+        id: 1,
+        raca: "Poodle"
+    },
+    {
+        id: 2,
+        raca: "Labrador"
+    }
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js");
+});
+
+app.get("/cachorros", (req, res) => {
+    res.status(200).json(cachorros);
 });
 
 export default app;
